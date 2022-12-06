bd118b1a5f9e7a16381e5ae798dadc71d43986b5
Sylvan <68489307+SylvanSun@users.noreply.github.com>
2022-12-06 20:30:33 +0800

    Create parser.py

---
+++ utils/parser.py
@@ -0,0 +1,16 @@
+import argparse
+
+def make_parser():
+    parser = argparse.ArgumentParser(description="Graph Matching Training Configurations")
+
+    parser.add_argument("--bs", type=int, default=32, help="batch size")
+    parser.add_argument("--lr", type=float, default=1e-4, help="learning rate")
+    parser.add_argument("--wd", type=float, default=1e-4, help="weight decay")
+    parser.add_argument("--ne", type=int, default=200, help="number of epochs")
+    parser.add_argument("--debug", action="store_true", help="run in debug mode")
+    return parser
+
+if __name__ == "__main__":
+    parser = make_parser()
+    args = parser.parse_args()
+    print(args)
